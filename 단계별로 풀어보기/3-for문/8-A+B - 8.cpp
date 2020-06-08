4bc603873d4a38150194611bb7cbbb41e84b0606
Katinon <66577670+Katinon@users.noreply.github.com>
2020-06-08 11:17:38 +0900

    Create 8-A+B - 8.cpp

---
+++ 단계별로 풀어보기/3-for문/8-A+B - 8.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int num, a, b;
+    cin >> num;
+    for (int i = 1; i <= num; i++) {
+        cin >> a >> b;
+        cout << "Case #" << i << ": " << a << " + " << b << " = " << a + b << "\n";
+    }
+}
