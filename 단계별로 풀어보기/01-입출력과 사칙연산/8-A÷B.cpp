9dc1fe37cc00b77754d4bcc2398731c3cc151643
Katinon <66577670+Katinon@users.noreply.github.com>
2020-06-09 19:23:20 +0900

    Create 8-A÷B.cpp

---
+++ 단계별로 풀어보기/01-입출력과 사칙연산/8-A÷B.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b;
+    cin >> a >> b;
+    cout.precision(15);
+    cout << a / b;
+    //double 타입은 부동소수점, float타입은 고정소수점
+}
