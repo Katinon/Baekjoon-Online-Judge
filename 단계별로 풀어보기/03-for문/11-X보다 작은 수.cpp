c75e2dd2898d27d41f594ea47556426d8c0f42e7
Katinon <66577670+Katinon@users.noreply.github.com>
2020-06-09 19:23:34 +0900

    Create 11-X보다 작은 수.cpp

---
+++ 단계별로 풀어보기/03-for문/11-X보다 작은 수.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int n, x, a;
+    cin >> n >> x;
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        if (a < x) {
+            cout << a << " ";
+        }
+    }
+}
