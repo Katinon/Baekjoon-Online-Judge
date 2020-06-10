bef930dfd57a43af2cfb099f640c9393ec1485a1
Katinon <66577670+Katinon@users.noreply.github.com>
2020-06-10 16:26:30 +0900

    Create 01-피보나치 5.cpp

---
+++ 단계별로 풀어보기/11-재귀/01-피보나치 5.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int Fibonacci(int num) {
+	if (num == 0 || num == 1) return (num == 0) ? 0 : 1;
+	return Fibonacci(num - 2) + Fibonacci(num - 1);
+}
+
+int main() {
+	int num;
+	cin >> num;
+	cout << Fibonacci(num);
+}
