b0c81b672e33a4f724b9cbd3f91c16ffbab546a8
Katinon <66577670+Katinon@users.noreply.github.com>
2020-06-10 16:28:48 +0900

    Create 01-팩토리얼.cpp

---
+++ 단계별로 풀어보기/11-재귀/01-팩토리얼.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+int Factorial(int n) {
+	if (n <= 1) return 1;
+	return n * Factorial(n - 1);
+}
+int main() {
+	int num;
+	cin >> num;
+	cout << Factorial(num);
+}
