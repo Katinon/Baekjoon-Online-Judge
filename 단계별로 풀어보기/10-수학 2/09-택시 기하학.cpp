d3704e6845de69ff99c8656d73776654e229f68b
Katinon <66577670+Katinon@users.noreply.github.com>
2020-06-10 14:22:33 +0900

    Create 09-택시 기하학.cpp

---
+++ 단계별로 풀어보기/10-수학 2/09-택시 기하학.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cmath>
+#define PI 3.1415926535897932
+using namespace std;
+
+int main() {
+    long double R;
+    cin >> R;
+    cout << fixed;
+    cout.precision(6);
+    cout << powl(R, 2) * PI << '\n';
+    cout << powl(R, 2) * 2 << '\n';
+    
+    //R의 값이 최대 10000이므로, define으로 pi를 지정하는경우
+    // 16번째자리까지는 적어주어야한다.
+}
