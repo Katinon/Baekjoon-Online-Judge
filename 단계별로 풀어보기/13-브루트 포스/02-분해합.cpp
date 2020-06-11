42fb4ce89118a3e5d773af67608d565c24923311
Katinon <66577670+Katinon@users.noreply.github.com>
2020-06-11 16:48:25 +0900

    Create 02-분해합.cpp

---
+++ 단계별로 풀어보기/13-브루트 포스/02-분해합.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int _sum(int n) {
+	int sum = 0;
+	while (n) {
+		sum += n % 10;
+		n /= 10;
+	}
+	return sum;
+}
+int ReturnNum2(int n) {
+	int min = 1000001;
+	for (int i = 0; i < 1000000; i++) {
+		int M = (i + 1) + _sum(i + 1);
+		if (M == n && min > i + 1) min = i + 1;
+	}
+	return (min == 1000001) ? 0 : min;
+}
+
+int main() {
+	int n;
+	cin >> n;
+	cout << ReturnNum2(n);
+}
