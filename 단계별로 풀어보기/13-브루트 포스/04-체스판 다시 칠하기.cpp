7c41e5383ceb5f9804359cdd721e4a546f3bcb0d
Katinon <66577670+Katinon@users.noreply.github.com>
2020-06-15 17:00:51 +0900

    Create 04-체스판 다시 칠하기.cpp

---
+++ 단계별로 풀어보기/13-브루트 포스/04-체스판 다시 칠하기.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+
+char B = 'B';
+char W = 'W';
+
+int main() {
+	int m, n;
+	cin >> m >> n;
+
+	char arr[51][51] = { NULL };
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> arr[i][j];
+		}
+	}
+	int cnt1=0, cnt2=0, min = 999999999;
+	int row = 0, column = 0;
+	for (int j = 0; j < m - 7; j++) { // j, k는 arr에서 8*8사이즈로 반복해야되는 횟수
+		column = 0;
+		for (int k = 0; k < n - 7; k++) {
+			for (int a = row; a < row + 8; a++) {
+				for (int b = column; b < column + 8; b++) {
+					if ((a % 2 == 0 && b % 2 == 0) || (a % 2 == 1 && b % 2 == 1)) {
+						if (arr[a][b] != B) cnt1++;
+						if (arr[a][b] != W) cnt2++;
+					}
+					if ((a % 2 == 0 && b % 2 == 1) || (a % 2 == 1 && b % 2 == 0)) {
+						if (arr[a][b] != W) cnt1++;
+						if (arr[a][b] != B) cnt2++;
+					}
+				}
+			}
+			if (min > cnt1&& cnt1 <= cnt2) min = cnt1;
+			else if (min > cnt2&& cnt2 <= cnt1) min = cnt2;
+			cnt1 = 0, cnt2 = 0;
+			column++;
+		}
+		row++;
+	}
+	cout << min;
+}
