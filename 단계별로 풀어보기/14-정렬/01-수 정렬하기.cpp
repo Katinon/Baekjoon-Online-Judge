54200ea0de070e150e7791aaac8038317c939f3e
Katinon <66577670+Katinon@users.noreply.github.com>
2020-07-06 17:26:01 +0900

    Create 01-수 정렬하기.cpp

---
+++ 단계별로 풀어보기/14-정렬/01-수 정렬하기.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int num[1000];
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++)
+		cin >> num[i];
+	for (int j = 0; j < N - 1; j++) {
+		for (int k = 0; k < N - j - 1; k++) {
+			int temp;
+			if (num[k] > num[k + 1]) {
+				temp = num[k];
+				num[k] = num[k + 1];
+				num[k + 1] = temp;
+			}
+		}
+	}
+	for (int i = 0; i < N; i++) cout << num[i] << endl;
+}
