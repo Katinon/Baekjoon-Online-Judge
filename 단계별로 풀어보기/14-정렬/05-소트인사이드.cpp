307ae8339c0de573f7f2affcf5233c15c3c500ac
Katinon <66577670+Katinon@users.noreply.github.com>
2020-08-06 15:01:46 +0900

    Create 05-소트인사이드.cpp

---
+++ 단계별로 풀어보기/14-정렬/05-소트인사이드.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void Swap(int* a, int* b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void SelectionSort(int a[], int cnt) {
+	for (int i = 0; i < cnt - 1; i++) {
+		for (int j = 0; j < cnt - 1 - i; j++) 
+			if (a[j] < a[j + 1]) Swap(&a[j], &a[j + 1]);
+	}
+}
+
+int main() {
+	int a, cnt=0; cin >> a;
+	int arr[10], temp = a;
+
+	for (int i = 0;; i++) {
+		cnt++;
+		arr[i] = temp % 10;
+		temp /= 10;
+		if (temp == 0) break;
+	}
+	SelectionSort(arr, cnt);
+	for (int i = 0; i < cnt; i++) cout << arr[i];
+}
