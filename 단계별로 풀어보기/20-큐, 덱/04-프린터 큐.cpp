7a7ffd121f982d9232112b64b781197885ccca3f
Katinon <66577670+Katinon@users.noreply.github.com>
2020-10-19 21:27:30 +0900

    Update 04-프린터 큐.cpp

--- 단계별로 풀어보기/20-큐, 덱/04-프린터 큐.cpp
+++ 단계별로 풀어보기/20-큐, 덱/04-프린터 큐.cpp
@@ -1,37 +1,30 @@
 #include <iostream>
-#include <queue>
 #include <vector>
 using namespace std;
 
 int main() {
 	int T; cin >> T;
 	for (int i = 0; i < T; i++) {
-		int N, M, imp, cnt = 0;
+		int N, M, cnt = 0;
 		cin >> N >> M;
 		vector<int> v, v_num;
-		
 		for (int i = 0; i < N; i++) {
 			int tmp; cin >> tmp;
 			v.push_back(tmp);
 			v_num.push_back(i);
-			if (M == i) {
-				imp = tmp;
-			}
 		} //input
 
 		while (true) {
-			vector<int>::iterator
-				iter1 = v.begin(), iter2 = v.begin();
 			int check = 0;
 			for (int i = 1; i < v.size(); i++) {
 				if (v[0] < v[i]) {
 					v.push_back(v[0]);
-					v.erase(iter1);
+					v.erase(v.begin());
 					v_num.push_back(v_num[0]);
-					v_num.erase(iter2);
+					v_num.erase(v_num.begin());
 					check = 1;
 					break;
-				}	
+				}
 			}
 			if (check == 0) {
 				cnt++;
@@ -40,11 +33,10 @@ int main() {
 					break;
 				}
 				else {
-					v.erase(iter1);
-					v_num.erase(iter2);
+					v.erase(v.begin());
+					v_num.erase(v_num.begin());
 				}
 			}
 		}
-
 	}
 }
