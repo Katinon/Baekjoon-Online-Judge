2374688040485583f81b14f37d9911b9d1140be6
Katinon <66577670+Katinon@users.noreply.github.com>
2020-12-28 00:16:47 +0900

    Create BOJ-10845.cpp

---
+++ Unclassified/BOJ-10845.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	string str;
+	queue<int> q;
+	int n; cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> str;
+		if (str == "push") {
+			int num; cin >> num;
+			q.push(num);
+		}
+		else if (str == "pop") {
+			if (q.empty()) cout << -1 << "\n";
+			else {
+				cout << q.front() << "\n";
+				q.pop();
+			}
+		}
+		else if (str == "size") {
+			cout << q.size() << "\n";
+		}
+		else if (str == "empty") {
+			if (q.empty()) cout << 1 << "\n";
+			else cout << 0 << "\n";
+		}
+		else if (str == "front") {
+			if (q.empty()) cout << -1 << "\n";
+			else cout << q.front() << "\n";
+		}
+		else if (str == "back") {
+			if (q.empty()) cout << -1 << "\n";
+			else cout << q.back() << "\n";
+		}
+	}
+}
