c55b2443db085534a87aa165b516150a1c1acdab
Katinon <66577670+Katinon@users.noreply.github.com>
2021-01-05 20:43:20 +0900

    Create BOJ-02440.cpp

---
+++ Unclassified/BOJ-02440.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int N; cin >> N;
+	for (int i = 0; i < N; i++) {
+		for (int j = N-i; j >= 1; j--) {
+			cout << "*";
+		}
+		cout << "\n";
+	}
+}
