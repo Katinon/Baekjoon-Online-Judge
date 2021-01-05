36d7295d89cb059ebe3237f05ae50d35c9d00a85
Katinon <66577670+Katinon@users.noreply.github.com>
2021-01-05 21:57:32 +0900

    Create BOJ-10178.cpp

---
+++ Unclassified/BOJ-10178.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int T; cin >> T;
+	for (int i = 0; i < T; i++) {
+		int c, v;
+		cin >> c >> v;
+		cout << "You get " << c / v << " piece(s) and your dad gets "
+			<< c % v << " piece(s).\n";
+
+	}
+}
